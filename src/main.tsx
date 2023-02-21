1a8b94bc48caaeddcf8ae0642d7bbd33fef873ac
Cinquin Andy <andy.cinquin@infotel.com>
2023-02-21 10:49:27 +0100

    footer & header adds

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/css/index.scss'
 import Routes from "./routes/Routes";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <Header/>
         <Routes/>
+        <Footer/>
     </React.StrictMode>,
 )
